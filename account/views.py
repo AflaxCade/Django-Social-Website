a2f9b66705e3b6ac8be60a5acf51f373453970e0
AflaxCade <aflah.cade@gmail.com>
2024-07-08 17:22:05 +0300

    Added user_list and user_detail views with login_required decorator

--- account/views.py
+++ account/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib import messages
 from .models import Profile
+from django.shortcuts import get_object_or_404
+from django.contrib.auth.models import User
 
 # Create your views here.
 
@@ -64,3 +66,13 @@ def edit(request):
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html',{'section': 'dashboard'})
+
+@login_required
+def user_list(request):
+    users = User.objects.filter(is_active=True)
+    return render(request,'account/user/list.html', {'section': 'people', 'users': users})
+
+@login_required
+def user_detail(request, username):
+    user = get_object_or_404(User, username=username, is_active=True)
+    return render(request, 'account/user/detail.html', {'section': 'people', 'user': user})
